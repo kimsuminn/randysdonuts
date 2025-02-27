63d7cd0c662373a713d4e04954d0165387241f0b
kimsuminn618 <kimsuminn618@gmail.com>
2025-02-27 20:34:33 +0900

    feat: menu page 제작완료

--- src/page/menu/Menu.tsx
+++ src/page/menu/Menu.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { useFetchMenu } from "../../hook/UseFetchMenu";
 import Title from "./components/Title";
 import "./style/Menu.css";
@@ -14,11 +14,27 @@ interface ItemType {
   best: boolean
 }
 
+interface CategoryType {
+  id: number,
+  menu: string,
+  url: string
+}
+
+const category :CategoryType[] = [
+  { id: 0, menu: 'Best', url: '/menu/best' },
+  { id: 1, menu: 'Classic', url: '/menu/classic' },
+  { id: 2, menu: 'Deluxe', url: '/menu/deluxe' },
+  { id: 3, menu: 'Fancy', url: '/menu/fancy' },
+  { id: 4, menu: 'Premium', url: '/menu/premium' },
+]
+
 function Menu() {
 
+  const { data } = useFetchMenu();
   const { pathname } = useLocation();
-  const [currentItem, setCurrentItem] = useState([]);
-  const { data, isLoading } = useFetchMenu();
+
+  const [currentItem, setCurrentItem] = useState<null | []>(null);
+  const [currentTitle, setCurrentTitle] = useState<string>('');
 
   let menu = pathname.split('/');
 
@@ -28,29 +44,35 @@ function Menu() {
         setCurrentItem(
           data?.filter((item :ItemType) => item.best)
         );
+        setCurrentTitle('랜디스도넛의 베스트 도넛을 만나보세요!');
         break;
       case 'classic':
         setCurrentItem(
           data?.filter((item :ItemType) => item.category === 'classic')
         );
+        setCurrentTitle('정통적인 재료로 기본에 집중한 클래식 도넛');
         break;
       case 'deluxe':
         setCurrentItem(
           data?.filter((item :ItemType) => item.category === 'deluxe')
         );
+        setCurrentTitle('미국 도넛의 진한 맛을 느낄 수 있는 디럭스 도넛');
         break;
       case 'fancy':
         setCurrentItem(
           data?.filter((item :ItemType) => item.category === 'fancy')
         );
+        setCurrentTitle('고급스러운 토핑이 아낌없이 들어간 팬시 도넛');
         break;
       case 'premium':
         setCurrentItem(
           data?.filter((item :ItemType) => item.category === 'premium')
         );
+        setCurrentTitle('수준 높은 재료와 형태를 가진 프리미엄 도넛');
         break;
       default:
-        setCurrentItem([]);
+        setCurrentItem(null);
+        setCurrentTitle('');
         break;
     }
   };
@@ -63,7 +85,47 @@ function Menu() {
     <div className='menu'>
       <Title />
       <div className="contents">
-
+        <div className="title">
+          <p className="main">도넛메뉴</p>
+          <p className="sub">World Famous Since 1952</p>
+        </div>
+        <ul className="category">
+          {
+            category.map(item => (
+              <li key={item.id} className={pathname === item.url ? "on" : ""}>
+                <Link to={item.url}>{item.menu}</Link>
+              </li>
+            ))
+          }
+        </ul>
+        <div className="title_img"></div>
+        <h4>{currentTitle}</h4>
+        <div className="item_box">
+          {
+            currentItem ?
+              currentItem.map((item :ItemType) => (
+                <div className="item" key={item.id}>
+                  <div className="info">
+                    {
+                      item.best ?
+                        <div className="icon">
+                          <p>BEST</p>
+                        </div> :
+                        <></>
+                    }
+                    <figure><img src={item.img} alt={item.name} /></figure>
+                    <p className="name">{item.name}</p>
+                  </div>
+                  <div className="hover">
+                    <h3>{item.name}</h3>
+                    <p className="sub">{item.en_name}</p>
+                    <p className="price">{item.price.toLocaleString('ko-KR')}원</p>
+                  </div>
+                </div>
+              )) :
+              <></>
+          }
+        </div>
       </div>
     </div>
   )
